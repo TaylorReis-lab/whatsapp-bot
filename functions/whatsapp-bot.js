3f3ec00f24ce2339015447afc5f6a151307ac98d
Taylor Reis <reist98822@gmail.com>
2024-07-28 11:22:23 -0300

    test menssage

--- functions/whatsapp-bot.js
+++ functions/whatsapp-bot.js
@@ -30,21 +30,64 @@ exports.handler = async (event, context) => {
   if (message.includes("oi")) {
     twiml.message("Olá! Como posso ajudar você hoje?");
   } else if (message.includes("ajuda")) {
-    twiml.message(
-      "Claro! Aqui está algumas opções: \n1. Informações sobre nós\n2. Suporte técnico\n3. Fale com um representante\n4. Ver nossos produtos"
+    const listMessage = {
+      to: parsedBody.From,
+      from: parsedBody.To,
+      body: "O que deseja fazer primeiro?",
+      persistentAction: ["geo:37.787890,-122.391664", "tel:+14155238886"],
+      action: "uri",
+      button: "Escolher",
+      sections: [
+        {
+          title: "Opções",
+          rows: [
+            {
+              id: "1",
+              title: "Informações sobre nós",
+            },
+            {
+              id: "2",
+              title: "Suporte técnico",
+            },
+            {
+              id: "3",
+              title: "Fale com um representante",
+            },
+            {
+              id: "4",
+              title: "Ver nossos produtos",
+            },
+          ],
+        },
+      ],
+    };
+    const client = twilio(
+      process.env.TWILIO_ACCOUNT_SID,
+      process.env.TWILIO_AUTH_TOKEN
     );
-  } else if (message.includes('1')) {
-    twiml.message('Informações sobre nós: Somos uma empresa dedicada a...');
-  } else if (message.includes('2')) {
-    twiml.message('Suporte técnico: Por favor, descreva seu problema técnico e nossa equipe irá ajudar.');
-  } else if (message.includes('3')) {
-    twiml.message('Fale com um representante: Conectando você a um representante...');
-  } else if (message.includes('4')) {
-    twiml.message('Ver nossos produtos: Aqui está a lista de nossos produtos...');
-  } else if (message.includes('')) {
-    twiml.message('Desculpe, não entendi sua mensagem. Por favor, escolha uma das opções do menu de ajuda.');
+    await client.messages.create(listMessage);
+    return {
+      statusCode: 200,
+      body: "",
+    };
+  } else if (message.includes("1")) {
+    twiml.message("Informações sobre nós: Somos uma empresa dedicada a...");
+  } else if (message.includes("2")) {
+    twiml.message(
+      "Suporte técnico: Por favor, descreva seu problema técnico e nossa equipe irá ajudar."
+    );
+  } else if (message.includes("3")) {
+    twiml.message(
+      "Fale com um representante: Conectando você a um representante..."
+    );
+  } else if (message.includes("4")) {
+    twiml.message(
+      "Ver nossos produtos: Aqui está a lista de nossos produtos..."
+    );
   } else {
-    twiml.message("Desculpe, não entendi sua mensagem.");
+    twiml.message(
+      "Desculpe, não entendi sua mensagem. Por favor, escolha uma das opções do menu de ajuda."
+    );
   }
 
   return {
