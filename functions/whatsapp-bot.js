cd6352828e15fa048a188d896407b5a9f72c69ab
Taylor Reis <reist98822@gmail.com>
2024-07-28 10:48:44 -0300

    test menssage

--- functions/whatsapp-bot.js
+++ functions/whatsapp-bot.js
@@ -1,16 +1,20 @@
 const twilio = require("twilio");
+const bodyParser = require("body-parser");
 
 exports.handler = async (event, context) => {
   const { MessagingResponse } = twilio.twiml;
   const twiml = new MessagingResponse();
 
-  // Verifique se event.queryStringParameters e event.queryStringParameters.Body existem
-  const message =
-    event.queryStringParameters && event.queryStringParameters.Body
-      ? event.queryStringParameters.Body.toLowerCase()
-      : "";
+  // Parse o corpo da requisição
+  const parsedBody = event.body ? JSON.parse(event.body) : {};
 
-  if (message.includes("oi")) {
+  // Verifique se parsedBody.Body existe
+  const message = parsedBody.Body ? parsedBody.Body.toLowerCase() : "";
+
+  // Adicionando logs para depuração
+  console.log("Mensagem recebida:", message);
+
+  if (message.includes("Ola" || "oi")) {
     twiml.message("Olá! Como posso ajudar você hoje?");
   } else if (message.includes("ajuda")) {
     twiml.message("Claro! Aqui está como posso ajudar...");
