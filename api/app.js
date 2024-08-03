228527b3aebd196c865a4ee49dadd093b98c56f0
Taylor Reis <reist98822@gmail.com>
2024-08-03 13:56:53 -0300

    reload version 1.0.1

--- api/app.js
+++ api/app.js
@@ -1,15 +1,13 @@
-import { SpeedInsights } from "@vercel/speed-insights/next";
-
 const twilio = require("twilio");
 const querystring = require("querystring");
+const { sendMessage, handleMenuSelection } = require("../functions/functions");
 
-<SpeedInsights />
-module.handler = async (event, context) => {
+module.exports = async (event, context) => {
   const { MessagingResponse } = twilio.twiml;
   const twiml = new MessagingResponse();
 
   let message = "";
-  let parsedBody = {};
+  let parsedBody = [];
 
   try {
     console.log("Raw body:", event.body);
@@ -31,62 +29,20 @@ module.handler = async (event, context) => {
 
   console.log("Mensagem recebida:", message);
 
-  try {
-    const client = twilio(
-      process.env.TWILIO_ACCOUNT_SID,
-      process.env.TWILIO_AUTH_TOKEN
+  const client = twilio(
+    process.env.TWILIO_ACCOUNT_SID,
+    process.env.TWILIO_AUTH_TOKEN
+  );
+
+  if (message.includes("oi")) {
+    const profileName = parsedBody.ProfileName || "usuário";
+    twiml.message(`Olá, ${profileName}! Como posso ajudar você hoje?`);
+  } else if (message.includes("ajuda")) {
+    twiml.message(
+      "Aqui estão algumas opções para melhor te ajudar:\n1. Informações sobre nós\n2. Suporte técnico\n3. Fale com um representante\n4. Ver nossos planos\n5. Nosso site"
     );
-
-    if (message.includes("oi")) {
-      const profileName = parsedBody.ProfileName || "usuário";
-      twiml.message(`Olá ${profileName},  Como posso ajudar você hoje?`);
-    } else if (message.includes("ajuda")) {
-      twiml.message(
-        "Aqui estão algumas opções para melhor te ajudar: \n1. Informações sobre nós\n2. Suporte técnico\n3. Fale com um representante\n4. Ver nossos planos\n5. Nosso site"
-      );
-    } else if (message.includes("1")) {
-      twiml.message("Informações sobre nós: Somos uma empresa dedicada a...");
-    } else if (message.includes("2")) {
-      twiml.message(
-        "Suporte técnico: Por favor, descreva seu problema técnico e nossa equipe irá ajudar."
-      );
-    } else if (message.includes("3")) {
-      const profileName = parsedBody.ProfileName || "usuário";
-      const userPhone = parsedBody.WaId;
-
-      twiml.message("Conectando você a um representante...");
-
-      const representativeNumber = "whatsapp:+556499833928";
-      const notificationMessage = `O usuário - ${profileName}, com o numero - (https://api.whatsapp.com/send?phone=${userPhone}), deseja falar com um representante.`;
-
-      try {
-        await client.messages.create({
-          body: notificationMessage,
-          from: parsedBody.To,
-          to: representativeNumber,
-        });
-      } catch (error) {
-        console.error("Error sending notification to representative:", error);
-      }
-    } else if (message.includes("4")) {
-      twiml.message(
-        "Planos mensal: 90 ReisPlano anul: 70 reias * 12 mesesAula esperimental: Agende já sua aula: <link para agendamento de aula experimental"
-      );
-    } else if (message.includes("5")) {
-      twiml.message(
-        "Acesse nosso site: (https://gran-fitness-site.netlify.app)"
-      );
-    } else {
-      twiml.message(
-        "Desculpe, não entendi sua mensagem. Por favor, escolha uma das opções do menu de ajuda."
-      );
-    }
-  } catch (error) {
-    console.error("Erro ao processar a mensagem:", error);
-    return {
-      statusCode: 500,
-      body: "Erro interno do servidor",
-    };
+  } else {
+    await handleMenuSelection(message, parsedBody, client, twiml);
   }
 
   return {
